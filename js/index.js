e225e789489904eedeb94026be6960a260f4adc4
insekkei <insekkei@yahoo.com>
2014-07-21 15:08:29 +0800

    use carousel

--- js/index.js
+++ js/index.js
@@ -20,26 +20,9 @@ $(function(){
 	$('.helloworld').click(function(e){
 		$(this).fadeOut(300);
 	});
-	ifFirstItem();
-	//slide left
-	$('#slide-left').click(function(e){
-		e.preventDefault();
-		var $curItem = $('.list-item.current');
-		$curItem.fadeOut(0).removeClass('current');
-		$curItem.prev().fadeIn(500).addClass('current');	
-		$('#slide-right').fadeIn(300);
-		ifFirstItem();
-	});
-	ifLastItem();
-	//slide right
-	$('#slide-right').click(function(e){
-		e.preventDefault();
-		var $curItem = $('.list-item.current');
-		$curItem.fadeOut(0).removeClass('current');
-		$curItem.next().fadeIn(500).addClass('current');	
-		$('#slide-left').fadeIn(300);
-		ifLastItem();
-	});
+
+	//slides
+	$('#carousel').carousel()
 	//contents
 	$(document).ajaxSend(function (e) {
 		$('#layout-cover').fadeIn(0);
@@ -50,6 +33,7 @@ $(function(){
 	$(document).ajaxError(function(){
 		//alert('找不到对应页面');
 	});
+
 	//from thumbnails of index to contents
 	$('a.thumbnail').click(function(e){
 		e.preventDefault();
@@ -85,23 +69,4 @@ $(function(){
 		}); 
 	}
 });
-
-//if current is first item
-function ifFirstItem(){
-	var $curItem = $('.list-item.current');
-	if($curItem.hasClass('first-item')){
-		$('#slide-left').fadeOut(0);
-	}else{
-		$('#slide-left').fadeIn(300);
-	}
-}
-//if current is last item
-function ifLastItem(){
-	var $curItem = $('.list-item.current');
-	if($curItem.hasClass('last-item')){
-		$('#slide-right').fadeOut(0);
-	}else{
-		$('#slide-right').fadeIn(300);
-	}
-}
 
